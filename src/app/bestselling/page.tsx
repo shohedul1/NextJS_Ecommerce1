d2790b533108170493ee238e711c7c0a8069bf32
Your Name <you@example.com>
2024-07-01 04:02:50 -0700

    shohidul

--- src/app/bestselling/page.tsx
+++ src/app/bestselling/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const bestselling = () => {
+const Bestselling = () => {
     useEffect(() => {
         AOS.init({
             offset: 100,
@@ -14,8 +14,8 @@ const bestselling = () => {
         AOS.refresh();
     }, []);
     return (
-        <div>bestselling</div>
+        <div>Bestselling</div>
     )
 }
 
-export default bestselling
+export default Bestselling;
